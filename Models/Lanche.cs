20877077fee2ee3530dfeb4e42452ab832cc47b9
Victor Dolsan <victorddf5@gmail.com>
2022-02-28 21:53:06 -0300

    Adicionando as models Categoria e Lanche

---
+++ Models/Lanche.cs
@@ -0,0 +1,19 @@
+namespace LanchesMAC.Models
+{
+    public class Lanche
+    {
+        public int LancheId { get; set; }
+        public string Name { get; set; }
+        public string DescricaoCurta { get; set; }
+        public string DescricaoDetalhada { get; set; }
+        public decimal Preco { get; set; }
+        public string ImagemUrl { get; set; }
+        public string ImagemThumbnailUrl { get; set; }
+        public bool IsLanchePreferido { get; set; }
+        public bool EmEstoque { get; set; }
+
+        public int CategoriaId { get; set; }
+        public virtual Categoria Categoria { get; set; }
+        
+    }
+}
