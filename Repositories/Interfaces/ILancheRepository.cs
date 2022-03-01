c345831fc9c39930ee00262aa3fec74c7573ebb0
Victor Dolsan <victorddf5@gmail.com>
2022-03-01 17:57:34 -0300

    Criado o LancheRepository e o ILancheRepository nas pastas Repositories e Interfaces

---
+++ Repositories/Interfaces/ILancheRepository.cs
@@ -0,0 +1,11 @@
+using LanchesMAC.Models;
+
+namespace LanchesMAC.Repositories.Interfaces
+{
+    public interface ILancheRepository
+    {
+         IEnumerable<Lanche> Lanches{ get; }
+         IEnumerable<Lanche> LanchesPreferidos { get; }
+         Lanche GetLancheById(int lancheId);
+    }
+}
---
+++ Repositories/LancheRepository.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using LanchesMAC.Repositories.Interfaces;
+using LanchesMAC.Context;
+using LanchesMAC.Models;
+
+namespace LanchesMAC.Repositories
+{
+    public class LancheRepository : ILancheRepository
+    {
+        private readonly AppDbContext _context;
+
+        public LancheRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Lanche> Lanches => _context.Lanches.Include(c => c.Categoria);
+        public IEnumerable<Lanche> LanchesPreferidos => _context.Lanches
+                                                        .Where(p => p.IsLanchePreferido)
+                                                        .Include(c => c.Categoria);
+        public Lanche GetLancheById(int lancheId)
+        {
+            return _context.Lanches.FirstOrDefault(l => l.LancheId == lancheId);
+        }        
+    }
+}
