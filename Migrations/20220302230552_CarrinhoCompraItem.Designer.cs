810b0748833f8b554b86718267be0c7c8fbba3ac
Victor Dolsan <victorddf5@gmail.com>
2022-03-02 20:09:39 -0300

    Adicionando a model CarrinhoCompraItem e feito o migration para o Banco de Dados

--- Migrations/20220302230552_CarrinhoCompraItem.Designer.cs
+++ Migrations/20220302230552_CarrinhoCompraItem.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using LanchesMAC.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LanchesMAC.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220302230552_CarrinhoCompraItem")]
+    partial class CarrinhoCompraItem
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Models/CarrinhoCompraItem.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LanchesMAC.Models
+{
+    [Table("CarrinhoCompraItens")]
+    public class CarrinhoCompraItem
+    {
+        public int CarrinhoCompraItemId { get; set; }
+        public Lanche Lanche { get; set; }
+        public int Quantidade { get; set; }
+
+        [StringLength(200)]
+        public string CarrinhoCompraId { get; set; }
+    }
+}
