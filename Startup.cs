48a3d27235fff268c34087905869b9f8f64b49fd
Victor Dolsan <victorddf5@gmail.com>
2022-03-02 20:36:30 -0300

    Adicionando a configuraçao de Session e HttpContext

--- Startup.cs
+++ Startup.cs
@@ -23,8 +23,13 @@ public class Startup
 
         services.AddTransient<ILancheRepository, LancheRepository>();
         services.AddTransient<ICategoriaRepository, CategoriaRepository>();
+        
+        services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
         services.AddControllersWithViews();
+
+        services.AddMemoryCache();
+        services.AddSession();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +49,8 @@ public class Startup
         app.UseStaticFiles();
 
         app.UseRouting();
+
+        app.UseSession();
 
         app.UseAuthorization();
 
