a61de9b1306865c8d8da78aac1f0677c5809f83a
Victor Dolsan <victorddf5@gmail.com>
2022-03-04 21:39:12 -0300

    Adicionado o metodo AdicionarAoCarrinho

--- Models/CarrinhoCompra.cs
+++ Models/CarrinhoCompra.cs
@@ -29,5 +29,29 @@ namespace LanchesMAC.Models
                 CarrinhoCompraId = carrinhoId
             };
         }
+
+        public void AdicionarAoCarrinho(Lanche lanche)
+        {
+            var carrinhoCompraItem = _context.CarrinhoCompraItens.SingleOrDefault(
+                s => s.Lanche.LancheId == lanche.LancheId &&
+                s.CarrinhoCompraId == CarrinhoCompraId
+            );
+
+            if(carrinhoCompraItem == null)
+            {
+                carrinhoCompraItem = new CarrinhoCompraItem
+                {
+                    CarrinhoCompraId = CarrinhoCompraId,
+                    Lanche = lanche,
+                    Quantidade = 1
+                };
+                _context.CarrinhoCompraItens.Add(carrinhoCompraItem);
+            }
+            else
+            {
+                carrinhoCompraItem.Quantidade++;
+            }
+            _context.SaveChanges();
+        }
     }
 }
