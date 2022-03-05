4e5af106bd5fe3a97ad354cc3b904d8f710ccd84
Victor Dolsan <victorddf5@gmail.com>
2022-03-04 21:29:41 -0300

    Adicionando a classe CarrinhoCompra

---
+++ Models/CarrinhoCompra.cs
@@ -0,0 +1,33 @@
+using LanchesMAC.Context;
+
+namespace LanchesMAC.Models
+{
+    public class CarrinhoCompra
+    {
+        private readonly AppDbContext _context;
+
+        public CarrinhoCompra(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public string CarrinhoCompraId { get; set; }
+        public List<CarrinhoCompraItem> CarrinhoCompraItems { get; set; }
+
+        public static CarrinhoCompra GetCarrinho(IServiceProvider services)
+        {
+            ISession session = services.GetRequiredService<IHttpContextAccessor>()?.HttpContext.Session;
+
+            var context = services.GetService<AppDbContext>();
+
+            string carrinhoId = session.GetString("CarrinhoId") ?? Guid.NewGuid().ToString();
+
+            session.SetString("CarrinhoId", carrinhoId);
+
+            return new CarrinhoCompra(context)
+            {
+                CarrinhoCompraId = carrinhoId
+            };
+        }
+    }
+}
