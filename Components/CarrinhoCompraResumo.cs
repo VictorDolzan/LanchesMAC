61f18c4ca9b1a7587a6dbdb7f916cd21d59ffe7f
Victor Dolsan <victorddf5@gmail.com>
2022-03-11 23:31:21 -0300

    Adicionando o Component CarrinhoResumo, e a view Default.cshtml que mostra o carrinho de compra

---
+++ Components/CarrinhoCompraResumo.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using LanchesMAC.Models;
+using LanchesMAC.ViewModels;
+
+namespace LanchesMAC.Components
+{
+    public class CarrinhoCompraResumo : ViewComponent
+    {
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public CarrinhoCompraResumo(CarrinhoCompra carrinhoCompra)
+        {
+            _carrinhoCompra = carrinhoCompra;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var itens = _carrinhoCompra.GetCarrinhoCompraItens();
+           
+            _carrinhoCompra.CarrinhoCompraItems = itens;
+
+            var carrinhoCompraVM = new CarrinhoCompraViewModel
+            {
+                CarrinhoCompra = _carrinhoCompra,
+                CarrinhoCompraTotal = _carrinhoCompra.GetCarrinhoCompraTotal()
+            };
+
+            return View(carrinhoCompraVM);
+        }
+    }
+}
