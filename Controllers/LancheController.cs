9e4de7de45dce7e932e704333b9ce91bd807a5d6
Victor Dolsan <victorddf5@gmail.com>
2022-03-15 20:15:20 -0300

    Editado a IActionResult List da LancheController

--- Controllers/LancheController.cs
+++ Controllers/LancheController.cs
@@ -14,13 +14,36 @@ namespace LanchesMAC.Controllers
         {
             _lancheRepository = lancheRepository;
         }
-        public IActionResult List()
+        public IActionResult List(string categoria)
         {
-            // var lanche = _lancheRepository.Lanches;
-            // return View(lanche);
-            var lanchesListViewModel = new LancheListViewModel();
-            lanchesListViewModel.Lanches = _lancheRepository.Lanches;
-            lanchesListViewModel.CategoriaAtual = "Categoria Atual";
+            IEnumerable<Lanche> lanches;
+            string categoriaAtual = string.Empty;
+
+            if(string.IsNullOrEmpty(categoria))
+            {
+                lanches = _lancheRepository.Lanches.OrderBy(l => l.LancheId);
+                categoriaAtual = "Todos os lanches";
+            }
+            else
+            {
+               if(string.Equals("Normal", categoria, StringComparison.OrdinalIgnoreCase))
+               {
+                   lanches = _lancheRepository.Lanches.Where(l => l.Categoria.CategoriaNome.Equals("Normal"))
+                                                      .OrderBy(l => l.Name);
+               }
+               else
+               {
+                   lanches = _lancheRepository.Lanches.Where(l => l.Categoria.CategoriaNome == "Natural")
+                                                      .OrderBy(l => l.Name);
+               }
+               categoriaAtual = categoria;
+            }
+
+            var lanchesListViewModel = new LancheListViewModel
+            {
+                Lanches = lanches,
+                CategoriaAtual = categoriaAtual
+            };
 
             return View(lanchesListViewModel);
         } 
