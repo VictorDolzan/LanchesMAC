ccbc71e038678c3cdc91b710e53aacd22bb1d8eb
Victor Dolsan <victorddf5@gmail.com>
2022-03-15 22:20:46 -0300

    Adicionando a CategoriaMenu, com o metodo Invoke criado

---
+++ Components/CategoriaMenu.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using LanchesMAC.Repositories.Interfaces;
+
+namespace LanchesMAC.Components
+{
+    public class CategoriaMenu : ViewComponent
+    {
+        private readonly ICategoriaRepository _categoriaRepository;
+
+        public CategoriaMenu(ICategoriaRepository categoriaRepository)
+        {
+            _categoriaRepository = categoriaRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categorias = _categoriaRepository.Categorias.OrderBy(c => c.CategoriaNome);
+            return View(categorias);
+        }
+
+    }
+}
