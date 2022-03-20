39355303b84955037395b455d8c0c3de5b631fe3
Victor Dolsan <victorddf5@gmail.com>
2022-03-20 20:49:48 -0300

    Criado o HTTPOST de Checkout na controller PeddoController

--- Controllers/PedidoController.cs
+++ Controllers/PedidoController.cs
@@ -24,7 +24,48 @@ namespace LanchesMAC.Controllers
         [HttpPost]
         public IActionResult Checkout(Pedido pedido)
         {
-            return View();
+            int totalItensPedido = 0;
+            decimal precoTotalPedido = 0.0m;
+
+            List<CarrinhoCompraItem> items = _carrinhoCompra.GetCarrinhoCompraItens();
+            _carrinhoCompra.CarrinhoCompraItems = items;
+
+            //verifica se existem itens de pedido
+            if(_carrinhoCompra.CarrinhoCompraItems.Count == 0)
+            {
+                ModelState.AddModelError("", "Seu carrinho está vazio, que tal incluir um lanche...");
+            }
+
+            //calcula o total de itens e o total do pedido
+            foreach(var item in items)
+            {
+                totalItensPedido += item.Quantidade;
+                precoTotalPedido += (item.Lanche.Preco * item.Quantidade);
+            }
+
+            //atribuir os valores obtidos ao pedido
+            pedido.TotalItensPedido = totalItensPedido;
+            pedido.PedidoTotal = precoTotalPedido;
+
+
+            //validar os dados do pedido
+            if(ModelState.IsValid)
+            {
+                //cria o pedido e os detalhes
+                _pedidoRepository.CriarPedido(pedido);
+
+                //define mensagens ao cliente
+                ViewBag.CheckoutCompletoMensagem = "Obrigado pelo seu pedido :)";
+                ViewBag.TotalPedido = _carrinhoCompra.GetCarrinhoCompraTotal();
+
+                //limpa o carrinho do cliente
+                _carrinhoCompra.LimparCarrinho();
+
+                //exibe view com dados do cliente e do pedido
+                return View("~/Views/Pedido/CheckoutCompleto.cshtml", pedido);
+            }
+            return View(pedido);
+
         }
     }
 }
