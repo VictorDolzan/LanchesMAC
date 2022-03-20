c6474b626712416790249140525e7cad5cbb2548
Victor Dolsan <victorddf5@gmail.com>
2022-03-20 12:58:55 -0300

    Criado o controlador PedidoController, com os metodos POST e GET

---
+++ Controllers/PedidoController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using LanchesMAC.Repositories.Interfaces;
+using LanchesMAC.Models;
+
+namespace LanchesMAC.Controllers
+{
+    public class PedidoController : Controller
+    {
+        private readonly IPedidoRepository _pedidoRepository;
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public PedidoController(IPedidoRepository pedidoRepository, CarrinhoCompra carrinhoCompra)
+        {
+            _pedidoRepository = pedidoRepository;
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+        [HttpGet]
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Pedido pedido)
+        {
+            return View();
+        }
+    }
+}
