74113b301cd8c3e3f8a4c98f10b438345b2aeaee
Victor Dolsan <victorddf5@gmail.com>
2022-04-09 22:59:28 -0300

    Criando um novo DB

---
+++ Migrations/20220410011927_AtualizacaoBanco.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LanchesMAC.Migrations
+{
+    public partial class AtualizacaoBanco : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
