9bbb291206653a4b8486a28c760747205b75bed7
Victor Dolsan <victorddf5@gmail.com>
2022-04-16 22:04:29 -0300

    Inserindo o Authorized(Admin) no AdminController e feito a addição do AddAuthorization no ConfigureServices

--- Areas/Admin/Controllers/AdminController.cs
+++ Areas/Admin/Controllers/AdminController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Authorization;
 namespace LanchesMAC.Areas.Admin.Controllers
 {
     [Area("Admin")]
-    [Authorize]
+    [Authorize("Admin")]
     public class AdminController : Controller
     {
         public IActionResult Index()
--- Startup.cs
+++ Startup.cs
@@ -44,6 +44,15 @@ public class Startup
         services.AddTransient<IPedidoRepository, PedidoRepository>();
         services.AddScoped<ISeedUserRoleInitial, SeedUserRoleInitial>();
 
+        services.AddAuthorization(options =>
+        {
+            options.AddPolicy("Admin",
+            politica =>
+            {
+                politica.RequireRole("Admin");
+            });
+        });
+
         services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
         services.AddScoped(sp => CarrinhoCompra.GetCarrinho(sp));
        
